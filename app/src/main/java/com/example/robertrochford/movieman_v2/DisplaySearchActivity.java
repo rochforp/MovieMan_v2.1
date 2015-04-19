749cad0ddff1642567ef0a26e76f8dbf916c3d32
Shahrar Nizam <shahrarnizam@shahrars-mbp-2.attlocal.net>
2015-04-19 17:27:36 -0500

    added jdbc connector and async

--- app/src/main/java/com/example/robertrochford/movieman_v2/DisplaySearchActivity.java
+++ app/src/main/java/com/example/robertrochford/movieman_v2/DisplaySearchActivity.java
@@ -1,36 +1,78 @@
 package com.example.robertrochford.movieman_v2;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
 
 
 public class DisplaySearchActivity extends ActionBarActivity {
-
+    TextView results;
+    String search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_display_search);
+        setContentView(R.layout.activity_display_search);
 
 
         //get the search from the intent in MainActivity.sendSearch
         Intent intent = getIntent();
-        String search = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
+        search = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        new LongOperation().execute(search);
         //create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(search);
+    }
 
-        //set textView as the activity layout
-        setContentView(textView);
+    private class LongOperation extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            results = (TextView) findViewById(R.id.result);
+            String response = "";
+            String url = "jdbc:mysql://98.130.0.90:3306/pggarla_movies";
+            String user = "pggarla_preader";
+            String pass = "Csc4610mysql";
+            System.out.println("searchMoviesByWord just went dawg");
+            try {
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                Connection conn = DriverManager.getConnection(url, user, pass);
+                Statement stmt = conn.createStatement();
+                System.out.println("Connected to database");
 
+                ResultSet rs = stmt.executeQuery("select distinct title.title from cast.info inner join title on cast_info.movie_id=title.id inner join name on cast_info.person.id=name.id where title.kind_id=1 and cast_info.role=1 and name.name like " + search);
+                String qres = "";
+                ResultSetMetaData rsmd = rs.getMetaData();
+                while (rs.next()) {
+                    qres += rsmd.getColumnName(1) + ": " + rs.getString(1) + "\n";
+
+                }
+                response = qres;
+            }
+            catch(java.sql.SQLException e1){
+                e1.printStackTrace();
+            }
+            catch(ClassNotFoundException | IllegalAccessException | InstantiationException e){
+                e.printStackTrace();
+            }
+            catch(Exception e){
+                System.out.println("Failed to connect to the database." + e.getMessage());
+            }
+            results.setText(response);
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            results.setText(result);
+        }
     }
 
 
